a1619da1aa6f9b6677e383e85ebaeccab5af9052
Filip Horvat <fico7489@gmail.com>
2018-01-06 11:48:14 +0100

    add many comments ...

--- src/Services/UpdateRelated.php
+++ src/Services/UpdateRelated.php
@@ -17,33 +17,47 @@ class UpdateRelated
 
         $configurations = self::prepareConfiguration($configurations);
 
+        // loop all configurations, ie the environments
         foreach ($configurations as $baseModel => $environments) {
             foreach ($environments as $environment) {
-                $name    = $environment['name'];
-                $related = $environment['related'];
 
-                $ids = collect();
-                foreach (self::$events as $modelEvent => $idsEvent) {
-                    if (array_key_exists($modelEvent, $related)) {
-                        $relation = $related[$modelEvent];
+                // environments name e.g. 'default'
+                $name = $environment['name'];
+
+                // mapping models to relations e.g. \App\Models\Address::class => 'addresses'
+                $modelRelationMapping = $environment['related'];
+
+                $modelIdsChanged = collect();
+
+                // in self::$events we have all changed models with ids, e.g. [App\Models\Address => [1, 2, 3]]
+                foreach (self::$events as $eventModel => $eventIds) {
+
+                    // check if we have changed model in package configuration
+                    if (array_key_exists($eventModel, $modelRelationMapping)) {
+
+                        // get relation so we can reach root model from changed model
+                        $relation = $modelRelationMapping[$eventModel];
                         
-                        $idsTmp = $baseModel::whereHas($relation, function ($query) use ($idsEvent) {
-                            return $query->whereIn('id', $idsEvent);
+                        $idsTmp = $baseModel::whereHas($relation, function ($query) use ($eventIds) {
+                            return $query->whereIn('id', $eventIds);
                         })->get()->pluck('id');
 
-                        $ids = $ids->merge($idsTmp);
+                        // now we have all changed model ids
+                        $modelIdsChanged = $modelIdsChanged->merge($idsTmp);
                     }
 
-                    if ($modelEvent == $baseModel) {
-                        $ids = $ids->merge($idsEvent);
+                    // if the changed model in the event is root model just get his ids
+                    if ($eventModel == $baseModel) {
+                        $modelIdsChanged = $modelIdsChanged->merge($eventIds);
                     }
                 }
-                $ids = $ids->unique()->sort();
-                
-                if (empty(self::$eventsProcessed[$baseModel][$name])) {
-                    self::$eventsProcessed[$baseModel][$name] = [];
-                }
-                self::$eventsProcessed[$baseModel][$name] += $ids->toArray();
+
+                // make changed ids unique and sort
+                $modelIdsChanged = $modelIdsChanged->unique()->sort();
+
+                // finally store all changed ids and models in $eventsProcessed
+                self::$eventsProcessed[$baseModel][$name] = ! empty(self::$eventsProcessed[$baseModel][$name]) ? self::$eventsProcessed[$baseModel][$name] : [];
+                self::$eventsProcessed[$baseModel][$name] += $modelIdsChanged->toArray();
             }
         }
         self::$events = [];
@@ -52,7 +66,8 @@ class UpdateRelated
     public static function fireEvents()
     {
         self::processEvents();
-        
+
+        // fire ModelChanged event for all change models and also for each environment
         foreach (self::$eventsProcessed as $baseModel => $environments) {
             foreach ($environments as $environmentName => $ids) {
                 foreach ($ids as $id) {
@@ -67,7 +82,6 @@ class UpdateRelated
     private static function prepareConfiguration($configurations)
     {
         // all configuration should have a name, but if there is no name we should give 'default' name so we would have all configurations uniform
-        
         foreach ($configurations as $baseModel => $environments) {
             foreach ($environments as $k => $environment) {
                 if (!array_key_exists('name', $environment)) {
